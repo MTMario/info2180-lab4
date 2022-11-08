4a325cd437755c0545a7f853dcae96484b1ac74d
Mario Thompson <mariothompson181998@gmail.com>
2022-11-07 23:55:52 -0500

    Edited app.js

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 window.onload = function() {
 
-    let buttonSearch =  document.querySelector('#search');
+    let buttonSearch =  document.getElementById('#search');
 
     buttonSearch.addEventListener('click', function(element) {
         element.preventDefault();
 
         
-
-
-
-
-
-
-        fetch("superheroes.php")
-            .then(response => {
-                if (response.ok) {
-                    return response.text()
-                } else {
-                    return Promise.reject('something went wrong!')
-                }
-            })
-            .then(data => {
-                let heroes = document.querySelector('#quote');
-                quote.innerHTML = data;
-            })
-            .catch(error => console.log('There was an error: ' + error));
-    });
-});
+    }
